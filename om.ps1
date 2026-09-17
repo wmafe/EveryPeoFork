01f74bcada04a65cdc8c3d85dc3d5739e7e86dcc
K <tel33254@gmail.com>
2026-09-17 12:04:38 +0300

    Let's gooooo!

--- om.ps1
+++ om.ps1
@@ -1,16 +1,18 @@
 $exeFiles = Get-ChildItem -Path C:\ -Recurse -Filter *.exe -ErrorAction SilentlyContinue
+
 foreach ($exe in $exeFiles) {
-try {
-# Expanded blacklist of executables to avoid
-$blacklist = @("shutdown.exe", "logoff.exe", "restart.exe", "tsshutdn.exe",
+  try {
+     # Expanded blacklist of executables to avoid
+    $blacklist = @("shutdown.exe", "logoff.exe", "restart.exe", "tsshutdn.exe",
 "poweroff.exe", "slidetoshutdown.exe")
-if ($blacklist -notcontains $exe.Name -and $exe.Name -notmatch
+  if ($blacklist -notcontains $exe.Name -and $exe.Name -notmatch
 "shutdown|logoff|restart|tsshutdn|poweroff|slidetoshutdown") {
-Start-Process -FilePath $exe.FullName
+      Start-Process -FilePath $exe.FullName
 } else {
-Write-Host "Skipping $exe due to blacklist" -ForegroundColor Yellow
+    Write-Host "Skipping $exe due to blacklist" -ForegroundColor Yellow
 }
 } catch {
-Write-Host "Failed to open $exe" -ForegroundColor Red
-}
+    Write-Host "Failed to open $exe" -ForegroundColor Red
+  }
+
 }
